8a4668c75a2b6a630c247a01fd65660d5bc1f9ad
Vinícius Oliveira <vinicius.oliveira1911@gmail.com>
2021-01-11 21:31:11 -0400

    Update main.py

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import pygame
 import os
 import time
 import random
+pygame.font.init()
 
 WIN_W, WIN_H = 650, 650
 WIN = pygame.display.set_mode((WIN_W, WIN_H))
@@ -24,10 +25,21 @@ BG = pygame.transform.scale(pygame.image.load(os.path.join("assets", "background
 def main():
     run = True
     FPS = 60
+    level = 1
+    lives = 3
+    main_font = pygame.font.SysFont("comicsans", 50)
+
     clock = pygame.time.Clock()
     
     def redraw_window():
         WIN.blit(BG, (0,0))
+        # EXIBR TEXTO NA TELA
+        lives_label = main_font.render(f'Vidas:{lives}', 1, (255,255,255))
+        level_label = main_font.render(f'Nível:{level}', 1, (255,255,255))
+
+        WIN.blit(lives_label, (20, 20))
+        WIN.blit(level_label, (WIN_W - level_label.get_width() - 20, 20))
+
         pygame.display.update()
 
     while run:
