38975d36d4026d3229278ace18405112229ed35b
Vinícius Oliveira <vinicius.oliveira1911@gmail.com>
2021-03-03 02:03:29 -0400

    Update main.py

--- main.py
+++ main.py
@@ -21,6 +21,7 @@ RED_LASER = pygame.image.load(os.path.join("assets", "pixel_laser_red.png"))
 GREEN_LASER = pygame.image.load(os.path.join("assets", "pixel_laser_green.png"))
 BLUE_LASER = pygame.image.load(os.path.join("assets", "pixel_laser_blue.png"))
 YELLOW_LASER = pygame.image.load(os.path.join("assets", "pixel_laser_yellow.png"))
+
 # BACKGROUND
 BG = pygame.transform.scale(pygame.image.load(os.path.join("assets", "background-black.png")), (WIN_W, WIN_H))
 
